ac9b33213df8db05c6afd68ff96f355ba28b77f3
H-YENER <H-YENER@HYENER>
2016-07-28 22:15:45 +0300

    test

--- JavaAnalysis/src/com/analysis/MainAnalysis.java
+++ JavaAnalysis/src/com/analysis/MainAnalysis.java
@@ -5,6 +5,7 @@ public class MainAnalysis {
 	public static void main(String[] args) {
 		// Test dosyalarý burada olacak
 		System.out.println("Test Main Analysis Project");
+		// Test yeni branch
 	}
 
 }
