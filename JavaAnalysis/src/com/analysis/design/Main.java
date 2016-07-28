f7f9423b6acecae7594ce0cfb07a1c2940324cae
H-YENER <H-YENER@HYENER>
2016-07-29 00:13:07 +0300

    window builder

---
+++ JavaAnalysis/src/com/analysis/design/Main.java
@@ -0,0 +1,59 @@
+package com.analysis.design;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.analysis.servicelayer.Analysis;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(97, 66, -58, -29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnTest = new JButton("Test");
+		btnTest.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Analysis obj = new 	Analysis(); 
+				obj.Test();
+			}
+		});
+		btnTest.setBounds(10, 11, 89, 23);
+		contentPane.add(btnTest);
+	}
+}
---
+++ JavaAnalysis/src/com/analysis/servicelayer/Analysis.java
@@ -0,0 +1,10 @@
+package com.analysis.servicelayer;
+
+import javax.swing.JOptionPane;
+
+public class Analysis {
+	public void Test()
+	{
+		JOptionPane.showMessageDialog(null, "My Goodness, this is so concise");
+	}
+}
