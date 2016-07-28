ebec789eb8829b6ece40ef592f82b5a1b2c0a5bc
hyener <huseyinyener1@hotmail.com>
2016-07-28 20:49:02 +0300

    test

--- JavaAnalysis/src/com/analysis/MainAnalysis.java
+++ JavaAnalysis/src/com/analysis/MainAnalysis.java
@@ -3,8 +3,9 @@ package com.analysis;
 public class MainAnalysis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Test dosyalarý burada olacak
 		System.out.println("Test Main Analysis Project");
+		
 	}
 
 }
